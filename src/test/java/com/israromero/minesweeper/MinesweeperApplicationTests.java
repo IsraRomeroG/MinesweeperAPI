8d4b3972f1f1d1e22a4e8b8dbe9ee474257dfa03
Isra Romero <israromero@outlook.com>
2021-04-26 10:19:01 -0500

    New empty proyect

---
+++ src/test/java/com/israromero/minesweeper/MinesweeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.israromero.minesweeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinesweeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
